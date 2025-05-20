fa9fff7b428f8def50112ba7e78a1c120f3a6208
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-05-20 15:22:44 -0300

    save adjusts

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-  
+
   const img = document.querySelector("#profile img")
 
-  if(html.classList.contains("light")) {
+  if (html.classList.contains("light")) {
 
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
